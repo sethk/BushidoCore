606746a05f7084f06ef2ab394d05c2d273e283e9
Seth Kingsley <sethk@meowfishies.com>
2011-12-12 00:09:16 -0800

    Add a table view subclass that turns typical key events into actions for a controller.

---
+++ BCTableView.h
@@ -0,0 +1,11 @@
+//
+//  BCTableView.h
+//  HTTPFlow
+//
+//  Created by Seth Kingsley on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+@interface BCTableView : NSTableView
+
+@end
---
+++ BCTableView.m
@@ -0,0 +1,51 @@
+//
+//  BCTableView.m
+//  HTTPFlow
+//
+//  Created by Seth Kingsley on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "BCTableView.h"
+
+@implementation BCTableView
+
+- (void)keyDown:(NSEvent *)theEvent
+{
+	BOOL eventHandled = NO;
+	NSString *characters = [theEvent characters];
+	if ([characters length] == 1)
+	{
+		switch ([characters characterAtIndex:0])
+		{
+			case NSBackspaceCharacter:
+			case NSDeleteCharacter:
+			{
+				id<NSTableViewDelegate> delegate = [self delegate];
+				if ([delegate respondsToSelector:@selector(remove:)])
+				{
+					[(id)delegate remove:self];
+					eventHandled = YES;
+				}
+				break;
+			}
+
+			case NSCarriageReturnCharacter:
+			case NSEnterCharacter:
+			{
+				id<NSTableViewDelegate> delegate = [self delegate];
+				if ([delegate respondsToSelector:@selector(add:)])
+				{
+					[(id)delegate add:self];
+					eventHandled = YES;
+				}
+				break;
+			}
+		}
+	}
+
+	if (!eventHandled)
+		[super keyDown:theEvent];
+}
+
+@end
