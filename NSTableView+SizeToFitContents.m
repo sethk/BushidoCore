0c98475aea5c334a3c716646b321a668db79b7ce
Seth Kingsley <sethk@meowfishies.com>
2011-12-23 14:36:39 -0800

    Fix the case where the table is wider than all columns combined.

--- NSTableView+SizeToFitContents.m
+++ NSTableView+SizeToFitContents.m
@@ -17,13 +17,26 @@
 	{
 		NSArray *columns = [self tableColumns];
 		NSUInteger numColumns = [columns count];
+		CGFloat totalWidth = 0;
 		for (NSUInteger columnIndex = 0; columnIndex < numColumns; ++columnIndex)
 		{
 			NSTableColumn *column = [columns objectAtIndex:columnIndex];
 			CGFloat columnWidth = [column minWidth];
 			for (NSUInteger rowIndex = 0; rowIndex < numRows; ++rowIndex)
-				columnWidth = MAX(columnWidth, [[self preparedCellAtColumn:columnIndex row:rowIndex] cellSize].width);
-			[column setWidth:columnWidth];
+			{
+				NSCell *cell = [self preparedCellAtColumn:columnIndex row:rowIndex];
+				columnWidth = MAX(columnWidth, [cell cellSize].width);
+			}
+			if (columnIndex < numColumns - 1)
+			{
+				[column setWidth:columnWidth];
+				totalWidth+= columnWidth;
+			}
+			else
+			{
+				[column setMinWidth:columnWidth];
+				[column setWidth:MAX(columnWidth, [[self superview] bounds].size.width - totalWidth)];
+			}
 		}
 	}
 	else
