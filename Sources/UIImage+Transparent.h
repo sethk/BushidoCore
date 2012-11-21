4e6730d9c4d3286139967887e20e83321fb26335
Seth Kingsley <sethk@meowfishies.com>
2012-11-20 17:21:03 -0800

    Add a category for creating arbitrarily-sized transparent images.

---
+++ Sources/UIImage+Transparent.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Transparent.h
+//  MBTI
+//
+//  Created by Seth Kingsley on 11/19/12.
+//  Copyright (c) 2012 Monkey Republic Design, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Transparent)
+
++ (UIImage *)transparentImageWithSize:(CGSize)size;
+
+@end
---
+++ Sources/UIImage+Transparent.m
@@ -0,0 +1,64 @@
+//
+//  UIImage+Transparent.m
+//  MBTI
+//
+//  Created by Seth Kingsley on 11/19/12.
+//  Copyright (c) 2012 Monkey Republic Design, LLC. All rights reserved.
+//
+
+#import "UIImage+Transparent.h"
+
+@implementation UIImage (Transparent)
+
+struct transparentBytesContext {off_t offset;};
+
+static size_t
+_getTransparentBytes(void *info, void *buffer, size_t count)
+{
+	bzero(buffer, count);
+	return count;
+}
+
+static off_t
+_skipTransparentBytes(void *info, off_t count)
+{
+	((struct transparentBytesContext *)info)->offset+= count;
+	return ((struct transparentBytesContext *)info)->offset;
+}
+
+static void
+_rewindTransparentBytes(void *info)
+{
+	((struct transparentBytesContext *)info)->offset = 0;
+}
+
+static void
+_releaseTransparentInfo(void *info)
+{
+	CFAllocatorDeallocate(kCFAllocatorDefault, info);
+}
+
++ (UIImage *)transparentImageWithSize:(CGSize)size
+{
+	CGDataProviderSequentialCallbacks callbacks =
+	{
+		.version = 0,
+		.getBytes = _getTransparentBytes,
+		.skipForward = _skipTransparentBytes,
+		.rewind = _rewindTransparentBytes,
+		.releaseInfo = _releaseTransparentInfo
+	};
+	struct transparentBytesContext *info = CFAllocatorAllocate(kCFAllocatorDefault, sizeof(*info), 0);
+	CGDataProviderRef dataProvider = CGDataProviderCreateSequential(info, &callbacks);
+	CGImageRef CGImage = CGImageCreate((size_t)size.width, (size_t)size.height,
+									   8, 32, (size_t)size.width * 4,
+									   CGColorSpaceCreateDeviceRGB(),
+									   kCGImageAlphaFirst | kCGBitmapByteOrderDefault,
+									   dataProvider,
+									   NULL,
+									   false,
+									   kCGRenderingIntentDefault);
+	return [[UIImage alloc] initWithCGImage:CGImage];
+}
+
+@end
